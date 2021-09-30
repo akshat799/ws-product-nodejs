3273b324e7be1e6c54301fe727d2fe90458c5e99
akshat799 <gulati.akshat799@gmail.com>
2021-09-30 04:56:00 -0300

    Changing CORS policy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('trust proxy' , true)
 
 app.use(function (req, res, next) {
   
-  res.setHeader('Access-Control-Allow-Origin', 'https://ws-product-nodejs.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.ALLOWCORS}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
